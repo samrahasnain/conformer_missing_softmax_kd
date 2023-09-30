7814431d4430a7838dbb09c5952de2b677c7ac1c
samrahasnain <143751042+samrahasnain@users.noreply.github.com>
2023-10-01 03:27:10 +0500

    Update distiller.py

--- distiller.py
+++ distiller.py
@@ -29,16 +29,17 @@ def build_feature_connector(t_channel, s_channel):
 class build_model_kd(nn.Module):
     def __init__(self, t_net, s_net):
         super(build_model_kd, self).__init__()
-        t=1
-        s=1
-        self.Connectors = build_feature_connector(t, s) 
+        t_channels=[1,1,1]
+        s_channels=[1,1,1]
+        self.Connectors = nn.ModuleList([build_feature_connector(t, s) for t, s in zip(t_channels, s_channels)])
         self.t_net = t_net
         self.s_net = s_net
 
     def forward(self, x,y):
         t1,t2,t3 = self.t_net(x,y)
         s1,s2,s3 = self.s_net(x,y)
-
+        t_feats=[t3,t2,t1]
+        s_feats=[s3,s2,s1]
         feat_num = len(t_feats)
         loss_distill = 0
         for i in range(feat_num):
@@ -46,4 +47,4 @@ class build_model_kd(nn.Module):
             #print(t_feats[i].shape,s_feats[i].shape)
             loss_distill += distillation_loss(s_feats[i], t_feats[i].detach())
 
-        return s1, loss_distill
+        return s1,s2,s3, loss_distill
